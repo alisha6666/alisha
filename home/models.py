8aa3c9545bedf786c4b27e9281b21a16c2269279
alisha6666 <alisha9shrestha@gmail.com>
2022-04-21 17:12:39 +0545

    price

--- home/models.py
+++ home/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
+
 # Create your models here.
+
+LABELS = (('new','new'), ('hot', 'hot'), ('sale', 'sale'),('','default'))
+class Category(models.Model):
+    name = models.CharField(max_length=500)
+    slug = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=500)
+    slug = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+class Slider(models.Model):
+    name = models.CharField(max_length=500)
+    image = models.ImageField(upload_to = 'media')
+    rank = models.IntegerField()
+    description = models.CharField(max_length=500, blank = True)
+    status = models.CharField(choices = (('active', 'active'), ('','default')), blank = True, max_length = 500)
+    
+
+    def __str__(self):
+        return self.name
+
+class Ad(models.Model):
+    name = models.CharField(max_length=500)
+    image = models.ImageField(upload_to = 'media')
+    rank = models.IntegerField()
+    status = models.CharField(choices = (('active', 'active'), ('','default')), blank = True, max_length = 500)
+
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=400)
+    price = models.IntegerField()
+    discounted_price = models.IntegerField()
+    image = models.ImageField(upload_to = 'media')
+    category = models.ForeignKey(Category,on_delete = models.CASCADE)
+    description = models.TextField(blank = True)
+    stock = models.CharField(max_length =50, 
+    choices = (('In Stock', 'In Stock'), ('Out of Stock', 'Out of Stock')))
+    labels = models.CharField(max_length = 50, choices = LABELS)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
+
+
--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+from django.views.generic import View
+
+class BaseView(View):
+    views = {}
+
+class HomeView(BaseView):
+    def get(self, request):
+        self.views['categories'] = Category.objects.all()
+        self.views['subcategories'] =Subcategory.objects.all()
+        self.views['sliders'] = Slider.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['products'] = Product.objects.filter(stock = 'In Stock')
+        self.views['sale_products'] = Product.objects.filter(labels ='sale', stock = 'In Stock')
+        self.views['new_products'] = Product.objects.filter(labels ='new', stock = 'In Stock')
+        self.views['hot_products'] = Product.objects.filter(labels ='hot', stock = 'In Stock')
+        return render(request, 'shop-index.html', self.views)
